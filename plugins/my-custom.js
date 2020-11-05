1bcfd162bc50934b2fa8faf09cb1b6119e48bd78
Isaac Ramirez <iramirezc@live.com.mx>
2020-11-04 23:24:20 -0600

    jsdoc config: - ignore docs folder - add jsdoc config file - install jsdoc as dev dependency - add yarn docs command - add custom plugin for jsdocs

---
+++ plugins/my-custom.js
@@ -0,0 +1,13 @@
+exports.handlers = {
+  parseComplete: function (e) {
+    console.log(`Total files processed: ${e.sourcefiles.length}`)
+  }
+}
+
+exports.defineTags = function (dictionary) {
+  dictionary.defineTag('note', {
+    onTagged: function (doclet, tag) {
+      doclet.description = `<em><strong>NOTE:</strong>&nbsp;${tag.text}</em>`
+    }
+  })
+}
