be45c683f95a960c94d5730fb14d344ec60e90d4
Isaac Ramirez <iramirezc@live.com.mx>
2020-11-06 02:10:04 -0600

    - add examples of @callback

---
+++ src/tags/callback.js
@@ -0,0 +1,15 @@
+/** @module tags/@callback */
+
+/**
+ * @callback myCallback
+ * @param {number[]} ids - An array of numbers representing the IDs
+ */
+
+/**
+ * @summary `@type {myCallback}`
+ * @description Example of declaring a variable to be a callback function.
+ * @example <caption>Calling the callback with the correct arguments.</caption>
+ * aCallbackFunction([1, 2, 3])
+ * @type {myCallback}
+ */
+const aCallbackFunction = function (ids) { /* noop */ }
