ddc6ad0356d00ca74df759fcf7b67d964b05c28f
Isaac Ramirez <iramirezc@live.com.mx>
2020-11-06 02:06:53 -0600

    - add examples of @typedef

---
+++ src/tags/typedef.js
@@ -0,0 +1,18 @@
+/** @module tags/@typedef */
+
+/**
+ * @typedef Vector
+ * @type {Object}
+ * @property {number} x - Point X
+ * @property {number} y - Point Y
+ */
+
+/**
+ * @summary `@type {Vector}`
+ * @description Example of a defined type `Vector` with properties `x` and `y` of type `number`.
+ * @example
+ * aVector = { x: 1, y: 0 }
+ * aVector = { x: -1, y: 3 }
+ * @type {Vector}
+ */
+let aVector = { x: 0, y: 0 }
