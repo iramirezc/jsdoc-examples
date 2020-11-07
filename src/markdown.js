2a6e2f035357234690bc9f503a6fa7b3455c4bfc
Isaac Ramirez <iramirezc@live.com.mx>
2020-11-07 13:57:31 -0600

    - add markdown examples

---
+++ src/markdown.js
@@ -0,0 +1,10 @@
+/**
+ * @description Example of how the **Markdown Plugin** works.
+ * @note This is my custom `@note` tag and should be recognized by the **Markdown Plugin** as well.
+ * @author Isaac Ramírez <iramirez@live.com.mx>
+ * @see [Using the Markdown plugin](https://jsdoc.app/plugins-markdown.html)
+ * @param {*} any - I will just **ignore** `any` params.
+ * @returns {undefined} It returns `undefined` by **default**.
+ * @throws Actually, it does not throw **anything**.
+ */
+function aMarkdownExample(any) { /* noop */ }
