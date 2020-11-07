c9c49cb00346e6cee72425746b65f726919c1e49
Isaac Ramirez <iramirezc@live.com.mx>
2020-11-07 16:01:07 -0600

    - refactor tutorial examples

--- src/tags/tutorial.js
+++ src/tags/tutorial.js
@@ -1,9 +1,17 @@
 /** @module tags/@tutorial */
 
 /**
- * @description Example of how to link my custom tutorials.
+ * @summary `@tutorial` block tag.
+ * @description Example of how to link my tutorials using the `@tutorial` block tag.
+ * @tutorial starter
+ */
+function blockTutorial () { /* noop */ }
+
+/**
+ * @summary `{@tutorial}` inline tag.
+ * @description Example of how to link my custom tutorials using the `@tutorial` inline tag.
  * You may also want to start with the {@tutorial starter} tutorial
  * or go ahead and start experimenting with {@tutorial command-line}.
  * @note [Learn more about this @note tag here]{@tutorial tags}.
  */
-function goToMyTutorials () { /* noop */ }
+function inlineTutorial () { /* noop */ }
